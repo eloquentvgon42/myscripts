100ee5a3fdb1621c5fdb42268ea0a596089deaa9
all powerful vogon leader <slackspam@gmail.com>
2016-03-29 22:59:13 -0400

    sec_tools_install.sh
    !

---
+++ sec_tools_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+usr/bin/apt-get install -y  nmap nbtscan wireshark tshark vlan dsniff tcpdump john p7zip openjdk-7-jre python 
+
+mkdir ~/tools
+
+cd ~/tools
+
+git clone https://github.com/makefu/dnsmap.git
+
+git clone https://github.com/Arachni/arachni.git
+
+git clone https://github.com/vs4vijay/heartbleed.git
+
+git clone https://github.com/urbanadventurer/WhatWeb.git
+
+git clone https://github.com/robertdavidgraham/masscan.git 
+
+cd ~/tools/masscan
+pip install pyip
+~/tools/masscan/bin
+make
+
